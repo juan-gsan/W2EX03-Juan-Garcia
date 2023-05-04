ea0e6a789df9c7d1e6c4695598851713082e85ed
Juan Garcia <juan.gsanchez13@gmail.com>
2023-05-04 13:17:28 +0200

    Add new test to vowels function

--- app/index.test.js
+++ app/index.test.js
@@ -1,10 +1,21 @@
 import checkAllVowels from "./index";
 
 describe("Given a checkAllVowels function", () => {
-  describe("When it receives the word 'murciélago'", () => {
+  describe("When it receives the word 'murcielago'", () => {
     test("Then it should return true", () => {
-      const word = "murciélago";
+      const word = "murcielago";
       const expectedOutput = true;
+
+      const hasAllVowels = checkAllVowels(word);
+
+      expect(hasAllVowels).toBe(expectedOutput);
+    });
+  });
+
+  describe("When it receives the word 'casa'", () => {
+    test("Then it should return false", () => {
+      const word = "casa";
+      const expectedOutput = false;
 
       const hasAllVowels = checkAllVowels(word);
 
